9b94c2d85921465a684072cddd6aa513c5b3fb08
Patricia Tigue <patriciatigue@patricias-imac.attlocal.net>
2021-04-16 02:50:24 -0500

    updates

--- js/Game.js
+++ js/Game.js
@@ -47,8 +47,8 @@
 *                    won
 */
    checkForWin(){
-     const letters = document.querySelectorAll('.letter')
-     const hideLetters =document.querySelectorAll('.hide')
+     const letters = document.querySelectorAll('.letter');
+     const hideLetters =document.querySelectorAll('.hide');
       for(let i = 0; i< letters.length; i++){
         if(hideLetters.length === 0){
           return true
@@ -58,22 +58,51 @@
     }
   }
 
+  /**
+  * Increases the value of the missed property
+  * Removes a life from the scoreboard
+  * Checks if player has remaining lives and ends game if player is out
+  */
+
   removeLife(){
     const tries = document.querySelectorAll('img');
-    tries[0].src =''
-    tries[0].src= 'images/lostHeart.png'
+    const overlay = document.querySelector('.start');
 
-    if(tries[0].src){
-      tries[1].src = ''
-      tries[1].src ='images/lostHeart.png'
-    }else if(tries[1].src){
-      tries[2].nextSiblingElement.src ='images/lostHeart.png'
-    }else if(tries[2].src ==='images/lostHeart.png'){
-      tries[2].nextSiblingElement.src ='images/lostHeart.png'
-    }else if(tries[3].src ==='images/lostHeart.png'){
-      tries[3].nextSiblingElement.src ='images/lostHeart.png'
-    }else if(tries[4].src ==='images/lostHeart.png'){
-      tries[4].nextSiblingElement.src ='images/lostHeart.png'
+    if(tries[0].getAttribute('src') === 'images/liveHeart.png' && tries[1].getAttribute('src') === 'images/liveHeart.png'){
+      tries[0].src =''
+      tries[0].src= 'images/lostHeart.png';
+    }else if(tries[1].getAttribute('src') === 'images/liveHeart.png' && tries[0].getAttribute('src') === 'images/lostHeart.png'){
+      tries[1].src =''
+      tries[1].src= 'images/lostHeart.png';
+    }else if(tries[2].getAttribute('src') === 'images/liveHeart.png' && tries[1].getAttribute('src') === 'images/lostHeart.png'){
+      tries[2].src =''
+      tries[2].src= 'images/lostHeart.png';
+    }else if(tries[3].getAttribute('src') === 'images/liveHeart.png' && tries[2].getAttribute('src') === 'images/lostHeart.png'){
+      tries[3].src =''
+      tries[3].src= 'images/lostHeart.png';
+    }else if(tries[4].getAttribute('src') === 'images/liveHeart.png' && tries[3].getAttribute('src') === 'images/lostHeart.png'){
+      tries[4].src =''
+      tries[4].src= 'images/lostHeart.png';
+    }  if(tries[4].getAttribute('src') === 'images/lostHeart.png'){
+        overlay.style.backgroundColor ='#ff8c00'
+        overlay.style.display ='block'
+        overlay.innerHTML =`<h2 class="title">Phrase Hunter</h2>
+				                    <h1 id="game-over-message">Sorry, better luck next time!</h1>
+				                    <button id="btn__reset">Start Game</button>`
     }
   }
- }
+/**
+* Displays game over message
+* @param {boolean} gameWon - Whether or not the user won the game
+*/
+  gameOver(gameWon){
+    if(this.checkForWin() === gameWon){
+      const overlay = document.querySelector('.start');
+      overlay.style.display ='block'
+      overlay.style.backgroundColor ='#77DD77'
+      overlay.innerHTML =`<h2 class="title">Phrase Hunter</h2>
+				                  <h1 id="game-over-message">Great Work</h1>
+				                  <button id="btn__reset">Start Game</button>`
+    }
+  }
+}
--- js/app.js
+++ js/app.js
@@ -1,13 +1,29 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
- let game;
+const keys= document.querySelectorAll('.key');
 
+ let game = new Game();
 
  document.querySelector('#btn__reset').addEventListener('click', e =>{
 
-   game =new Game()
+
    game.startGame()
   e.preventDefault()
- })
-;
+ });
+
+  keys.forEach(key => {
+    key.addEventListener('click', function handleInteraction(e) {
+    if(game.activePhrase.checkLetter(e.target.textContent) === false || !game.activePhrase.phrase.includes(e.target.textContent)){
+        e.target.classList.add('lose');
+        game.removeLife();
+    } if( game.activePhrase.showMatchedLetter(e.target.textContent) || game.activePhrase.checkLetter(e.target.textContent) === true || game.activePhrase.phrase.includes(e.target.textContent)){
+
+          e.target.classList.add('chosen');
+
+    }
+    if(game.checkForWin()){
+
+    }
+  })
+})
